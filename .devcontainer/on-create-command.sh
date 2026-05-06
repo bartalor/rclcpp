f22d6aae8f808db21312cdcf46b581f23425c349
Bar <bartalor@gmail.com>
2026-05-06 13:31:19 +0300

    devcontainer: move rosdep update to runtime, keep apt cache
    rosdep DB stales between image build and use; update at container
    start instead. Drop the apt-lists rm in on-create-command since the
    docker-clean trick exists to keep them.
    
    Drop colcon mixin/metadata updates: only stock mixins are used and
    those ship with python3-colcon-mixin / -metadata.

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -5,10 +5,10 @@ git config --global --add safe.directory "*"
 
 # install overlay deps from mounted source
 . /opt/ros/$ROS_DISTRO/setup.sh
+rosdep update
 apt-get update
 rosdep install -q -y \
     --from-paths $OVERLAY_WS/src \
     --ignore-src
-rm -rf /var/lib/apt/lists/*
 
 .devcontainer/update-content-command.sh
