87ab6c030ebff6d5c78aa282de5500830f78fc1a
Bar <bartalor@gmail.com>
2026-05-25 09:01:38 +0000

    devcontainer: copy host ~/.netrc into container
    Lets curl --netrc inside the container authenticate to ci.ros2.org
    without exposing the token in env vars or logs.

--- .devcontainer/post-rebuild-host-setup.sh
+++ .devcontainer/post-rebuild-host-setup.sh
@@ -57,3 +57,4 @@ copy_if_changed() {
 
 copy_if_changed "$HOME/dotfiles/path_scripts/.local/bin/git-Pretty" /usr/local/bin/git-Pretty
 copy_if_changed "$HOME/dotfiles/bash/.bashrc.d/git-completions.sh" /etc/bash_completion.d/git-completions.sh
+copy_if_changed "$HOME/.netrc" /home/ubuntu/.netrc
