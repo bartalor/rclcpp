27902016af5f5890f3b04ef3c803c2cac6d3eec9
Bar <bartalor@gmail.com>
2026-06-19 15:27:57 +0300

    devcontainer: disable basic-memory plugin

--- .devcontainer/plugins.sh
+++ .devcontainer/plugins.sh
@@ -2,9 +2,7 @@
 # post-create-command.sh. Edit the array to enable/disable plugins;
 # a rebuild is needed for plugins that contribute a compose.yaml.
 PLUGINS=(
-    basic-memory
     mcp-cpp
     open-source-utils
     git-graph-2
-    # mcp-memory
 )
